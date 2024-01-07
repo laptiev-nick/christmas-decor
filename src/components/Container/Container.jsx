bbeb921f8b635266bdd28bc1fc1174128588aab7
laptiev-nick <135578389+laptiev-nick@users.noreply.github.com>
2024-01-07 15:57:34 +0200

    updated files

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,8 +1,8 @@
 import styles from './Container.module.scss';
 
-export const Container = ({children}) => {
+export const Container = ({ children, style }) => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} style={style}>
             {children}
         </div>
     )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,9 @@
-import styles from "./Header.module.scss";
+import { TfiFacebook } from "react-icons/tfi";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
 import { Container } from "components/Container/Container";
 import { Logo } from "../Logo/Logo";
+import styles from "./Header.module.scss";
 
 export const Header = () => {
     return (
@@ -20,19 +23,45 @@ export const Header = () => {
             </Container>
             <nav className={styles.navigation}>
                 <Container>
-                <Logo />
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    </ul>
+                    <div className={styles.bottomPanel}>
+                        <Logo />
+                        <div className={styles.listsWrapper}>
+                            <ul className={styles.menuList}>
+                                <li className={styles.menuItem}>
+                                    <a className={styles.menuLink} href="#">Bestsellers</a>  
+                                </li>
+                                <li className={styles.menuItem}>
+                                    <a className={styles.menuLink} href="#">About us</a>
+                                </li>
+                                <li className={styles.menuItem}>
+                                    <a className={styles.menuLink} href="#">Decorations</a>
+                                </li>
+                                <li className={styles.menuItem}>
+                                    <a className={styles.menuLink} href="#">FAQ</a>
+                                </li>
+                                <li className={styles.menuItem}>
+                                    <a className={styles.menuLink} href="#">Contacts</a>
+                                </li>
+                            </ul>
+                            <ul className={styles.socialsList}>
+                                <li className={styles.socialsItem}>
+                                    <a className={styles.socialsLink} href="#">
+                                        <TfiFacebook className={styles.socialsIcon}/>   
+                                    </a>
+                                </li>
+                                <li className={styles.socialsItem}>
+                                    <a className={styles.socialsLink} href="#">
+                                        <FaInstagram className={styles.socialsIcon}/>        
+                                    </a>
+                                </li>
+                                <li className={styles.socialsItem}>
+                                    <a className={styles.socialsLink} href="#">
+                                        <FaYoutube className={styles.socialsIcon}/>
+                                    </a> 
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </Container>
             </nav>
         </header>
